314e2681874a74f940503b355868fccdc4dcfe16
Lightfooted <vesselofbalance@hotmail.com>
2021-07-10 01:23:26 -0400

    removed concat for addition assignment, added math.random

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function generatePassword() {
   var lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var storage = [""];
-  var finalPassword = [""]
+  var finalPassword = [""];
 
   //Generate Prompt
   var passwordLength = prompt("How long would you like your password to be? The password must be between 8 to 128 characters long.");
@@ -32,21 +32,24 @@ function generatePassword() {
   };
   var nosNumber = confirm("Would you like to add numbers?");
   if (nosNumber){
-    storage = storage.concat(nos);
+    storage += nos;
   }
+
   var lowerLetter = confirm("Would you like to add lowercased letters?");
   if (lowerLetter){
-    storage = storage.concat(lowerLetter);
+    storage += lowerLetter;
   }
 
   var upperLetter = confirm("Would you like to add uppercased letters?");
   if (upperLetter){
-    storage = storage.concat(upperLetter);
+    storage += upperLetter;
   }
 
   var specialCharacter = confirm("Would you like to add special characters?")
   if (specialCharacter){
-    storage = storage.concat(special);
+    storage += special;
   }
-
+  for (var i = 0; i < passwordLength; i++) {
+    password = storage[Math.floor(Math.random() * storage.length)]
+  }
 };
